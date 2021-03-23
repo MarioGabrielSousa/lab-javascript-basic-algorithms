393e46f95cf7d3fa2f9c896be7550574957e8c53
mario sousa <vegetalizado@gmail.com>
2021-03-23 09:57:48 +0000

    Basic Algorythm lab

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
 // Iteration 1: Names and Input
-let hacker1 = "Mário";
-console.log(`The driver\'s name is ${hacker1}`);
-let hacker2 = "Guilherme";
-console.log(`The navigator\'s name is ${hacker2}`);
+const hacker1 = "Mário";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Dhiren";
+console.log(`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
-if(hacker1.length > hacker2.length) {
-  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
-} else if (hacker1.length < hacker2.length) {
-  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters`
+  );
+} else if (hacker2.length > hacker1.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters`
+  );
 } else {
-  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+  console.log(`Wow, you both have equally long names, ${hacker1} characters!`);
 }
 
-// Iteration 3: Loops
-//3 exercicio
-//(usar loop for, e nesse for iterações do tamanho da string initial (hacker1)
-let string = "";
-
-
-//(usar loop for, e nesse for iterações do tamanho da string initial (hacker1)
-for (let i =0; i < hacker1.length; i++)  {
- let tempString = hacker1.toUpperCase();
- string  += tempString[i] + " ";
+//Iteration 3: Loops
+let auxiliaryString = "";
+for (let i = 0; i < hacker1.length; i++) {
+  auxiliaryString += hacker1[i].toUpperCase() + " ";
+  console.log(auxiliaryString);
 }
-console.log(string);
+console.log(auxiliaryString);
+
+let reverseName = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reverseName += hacker2[i];
+}
+console.log(reverseName);
+
+let alphabetName = hacker1.localeCompare(hacker2);
+if (alphabetName === -1) {
+  console.log("The driver's name goes first");
+} else if (alphabetName === 1) {
+  console.log("Yo, the navigator goes first definitely");
+} else {
+  console.log("What?! You both have the same name?");
+}
